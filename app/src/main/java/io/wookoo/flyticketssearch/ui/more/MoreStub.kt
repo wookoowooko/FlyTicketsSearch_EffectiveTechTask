f04795aa11fa300f6fd836295bf8706ba019ec6c
genry <renegadeworking@gmail.com>
2024-12-05 18:53:57 +0300

    fix id

--- app/src/main/java/io/wookoo/flyticketssearch/ui/more/MoreStub.kt
+++ app/src/main/java/io/wookoo/flyticketssearch/ui/more/MoreStub.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import io.wookoo.flyticketssearch.databinding.FragmentMoreBinding
-import io.wookoo.flyticketssearch.databinding.FragmentNotificationsBinding
 
 class MoreStub : Fragment() {
 
